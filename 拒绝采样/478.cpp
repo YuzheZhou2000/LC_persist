b046f2e2e4d6a1daaaf065baba0c5f11c2754e6b
‘zhouyuzhe’ <zhou_yuz@163.com>
2022-10-17 13:57:29 +0800

    478

--- 拒绝采样/478.cpp
+++ 拒绝采样/478.cpp
@@ -1,1 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+int main()
+{
+    int i = 0;
+    do{
+        cout << i++ << endl;
+    } while (i < 10);
+
+    return 0;
+}
